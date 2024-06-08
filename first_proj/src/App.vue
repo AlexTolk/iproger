f32c813a0c344e28909b3ff495a46fc13a1cb657
AlexTolk <alexandr.tolkachyov@gmail.com>
2024-06-08 22:27:53 +0800

    first commit

---
+++ first_proj/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+  export default {
+    data() {
+      return {
+        info: 'Lorem',
+        moreInfo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi natus id reiciendis?'
+      }
+    },
+      methods: {
+        userData() {
+          this.info = 'Changed the info'
+        }
+    }
+  }
+
+</script>
+
+<template>
+  <body>
+    <h3>{{ info }}</h3>
+    <p>{{ moreInfo }}</p>
+    <button type="button" @click="userData()">Change</button>
+  </body>
+</template>
+
+<style scoped>
+  h3 {
+    font-weight: lighter;
+    color:aliceblue;
+  }
+  p {
+    color:crimson;
+  }
+</style>
