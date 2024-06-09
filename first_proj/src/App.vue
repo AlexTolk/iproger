b878a1981774333ce54bb0b93157f9cf5ac47127
AlexTolk <alexandr.tolkachyov@gmail.com>
2024-06-09 20:39:28 +0800

    added User component

--- first_proj/src/App.vue
+++ first_proj/src/App.vue
@@ -1,34 +1,56 @@
-<script>
-  export default {
-    data() {
-      return {
-        info: 'Lorem',
-        moreInfo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi natus id reiciendis?'
-      }
-    },
-      methods: {
-        userData() {
-          this.info = 'Changed the info'
-        }
-    }
-  }
-
-</script>
-
 <template>
   <body>
-    <h3>{{ info }}</h3>
-    <p>{{ moreInfo }}</p>
-    <button type="button" @click="userData()">Change</button>
+    <form action="">
+      <input type="text" v-model="userName" placeholder="name">
+      <input type="password" v-model="userPassword" placeholder="password">
+      <input type="email" v-model="userEmail" placeholder="email">
+      <p className="error">{{ error }}</p>
+      <button @click="sendData()">Send</button>
+    </form>
+
+    <User v-for="(el, i) in users" :key="i" :user="el" :deleteUser="deleteUSer"/>
+
   </body>
 </template>
 
+<script>
+  import User from './components/User.vue'
+  export default {
+    components: { User },
+    data() {
+      return {
+        users: [],
+        error: '',
+        userName: '',
+        userPassword: '',
+        userEmail: ''
+      }
+    },
+      methods: {
+        sendData() {
+          if(this.userName == '') {
+            this.error = 'Name not submited';
+            return
+          } else if(this.userPassword == '') {
+            this.error = 'Password not submited';
+            return
+          } else if(this.userEmail == '') {
+            this.error = 'Email not submited';
+            return
+          }
+          this.error = ''
+          this.users.push({
+            name: this.userName,
+            password: this.userPassword,
+            email: this.userEmail
+          })
+      },
+      deleteUser(index) {
+        this.user.splice(index, 1)
+      }
+    }
+  }
+</script>
+
 <style scoped>
-  h3 {
-    font-weight: lighter;
-    color:aliceblue;
-  }
-  p {
-    color:crimson;
-  }
 </style>
---
+++ first_proj/src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    <button @click="deleteUser(index)">Delete</button>
+    <h3>{{ user.name }}</h3>
+    <p>{{ user.email }} -- {{ user.password }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        required: true
+      }
+    },
+    index: {
+      type: Number,
+      required: true
+    },
+    deleteUser: {
+      type: Function,
+      required: true
+    }
+  }
+</script>
+
+<style scoped>
+</style>
